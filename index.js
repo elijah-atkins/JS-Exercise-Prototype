68d06e8ba1aa48046a306922eabbb2c0874c1a66
elijah-atkins <elijahorama@gmail.com>
2020-02-15 15:42:46 -0700

    started challenge

--- index.js
+++ index.js
@@ -39,10 +39,19 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
-
+Person.prototype.eat = function(food){
+  if (this.stomach.length < 10){
+    this.stomach.push(food);
+  }
+}
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
 /*
   TASK 2
     - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
